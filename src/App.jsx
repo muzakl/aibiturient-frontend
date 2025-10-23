df6b698fd2a83360ee68ea8d9ecfe0c816f60215
Giorgi Muzashvili <162874503+muzakl@users.noreply.github.com>
2025-10-23 12:37:05 +0400

    5: Created basic routing system and linked both pages (#6)

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import HomePage from './pages/HomePage/HomePage'
-
-const App = () => {
-  // return <LandingPage />
-  return <HomePage />
-}
-
-export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import styles from './Header.module.css'
 import PFP from '../../assets/Hojlund.jpg'
+import { Link } from 'react-router'
 
 const Header = () => {
   return (
     <div className={styles.headerDiv}>
-      <h1 className={styles.headerText}>
-        <span className={styles.headerSpan}>AI</span>Biturient
-      </h1>
+      <Link to="/" className={styles.link}>
+        <h1 className={styles.headerText}>
+          <span className={styles.headerSpan}>AI</span>Biturient
+        </h1>
+      </Link>
       <button className={styles.profileButton}>
         {/* Using static image for now, replace with actual user PFP later. */}
         <img src={PFP} alt="Profile picture" />
